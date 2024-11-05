5f9b54d342d0aaa5b60b37f859af3c9ee2d3cc7c
Gnana Sarathy <gnanasarathy.alagesan@cdw.com>
2024-11-05 17:16:48 +0530

    |Saarathy|Initial commit

---
+++ constants/generic.py
@@ -0,0 +1,14 @@
+# constants.py
+STAGE = "stage"
+OUTPUT = "output"
+SOURCE = "source"
+TEMPLATE = "template"
+
+from enum import Enum
+
+class Office(Enum):
+    CHENNAI = 'CHE'
+    HYDERABAD = 'HYD'
+    BANGLORE = 'BLR'
+
+
